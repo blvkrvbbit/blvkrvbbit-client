4113614ebbfad8bd4552a75814b7d40a2f9e0ed5
Jordan Myers <blvkrvbbit@gmail.com>
2024-09-15 20:15:51 -0600

    Started front end

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,105 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
+import ScrollSnap from '@/components/scroll-snap/scroll-snap.component';
+import ScrollSnapSection from '@/components/scroll-snap/scroll-snap-section/scroll-snap-section.component';
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <ScrollSnap>
+        <section className='hero z-5'>
+          <div className='relative top-[7.3rem] h-[80vh]'>
+            <div className='space-y-2'>
+              <div className='text-gray opacity-[0.15]'>{`<h1>`}</div>
+              <h1 className='pl-4 tracking-wide text-4xl md:text-6xl'>
+                blvk<span className='text-purple'>.rabbit()</span>
+              </h1>
+              <div className='text-gray opacity-[0.15]'>{`</h1>`}</div>
+            </div>
+            <h2 className='mt-2 tracking-widest font-semibold'>
+              Frontend
+              <span className='text-purple'> /</span> Backend Developer
+            </h2>
+            <div className='absolute bottom-0 flex justify-center items-center w-full text-5xl'>
+              <Link href='#works'>
+                <Icon
+                  className='text-purple'
+                  icon='fluent:chevron-down-20-filled'
+                />
+              </Link>
+            </div>
+          </div>
+        </section>
+        <ScrollSnapSection title={'works'}>
+          <div className='text-2xl mb-6 font-semibold tracking-widest'>
+            <span className='text-gray opacity-[0.15]'>.</span>works
+            <span className='text-gray opacity-[0.15]'>()</span>
+          </div>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+            <div className='border h-[15rem] flex justify-center items-center'>
+              Placeholder
+            </div>
+          </div>
+        </ScrollSnapSection>
+        <ScrollSnapSection title={'about'}>
+          <div className='text-2xl mb-4 font-semibold tracking-widest'>
+            <span className='text-gray opacity-[0.15]'>.</span>about
+            <span className='text-gray opacity-[0.15]'>()</span>
+          </div>
+          <div className='space-y-4'>
+            <p className='max-w-prose'>
+              Hi, I&apos;m Jordan Myers, a passionate web developer with a knack
+              for creating dynamic and responsive web applications. With
+              extensive experience in{' '}
+              <span className='text-purple font-semibold'>React</span>,{' '}
+              <span className='text-purple font-semibold'>Next.js</span>, and
+              <span className='text-purple font-semibold'> TypeScript</span>, I
+              thrive on building user-friendly interfaces and solving complex
+              problems.
+            </p>
+            <p className='max-w-prose'>
+              I believe in the power of teamwork and enjoy collaborating with
+              others to bring innovative ideas to life. Outside of coding,
+              you&apos;ll often find me at tech meet-ups, staying up-to-date
+              with the latest advancements in technology. I&apos;m also a big
+              fan of games and am an avid coffee drinker.
+            </p>
+            <p className='max-w-prose'>
+              Whether it&apos;s coding, learning, or just enjoying a good cup of
+              coffee, I&apos;m always eager to connect and share experiences.
+              Let&apos;s create something amazing together!
+            </p>
+          </div>
+        </ScrollSnapSection>
+        <ScrollSnapSection title={'contact'}>
+          <div className='text-2xl mb-4 font-semibold tracking-widest'>
+            <span className='text-gray opacity-[0.15]'>.</span>contact
+            <span className='text-gray opacity-[0.15]'>()</span>
+          </div>
+        </ScrollSnapSection>
+      </ScrollSnap>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,63 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      animation: {
+        grid: 'grid 15s linear infinite',
+      },
+      keyframes: {
+        grid: {
+          '0%': { transform: 'translateY(-50%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        ...colors,
+        // Configure your color palette here
+        onyx: '#474448',
+        white: '#ffffff',
+        gray: '#848C8E',
+        purple: '#9730db',
+        black: '#272932',
+        keppel: '#1ac5b0',
+        blue: '#37ebf3',
+        yellow: '#fdf500',
+        pink: '#e455ae',
+        ['smoky-black']: '#0e1212',
+        ['steel-pink']: '#cb1dcd',
+        ['pale-silver']: '#d1c5c0',
+        green: '#06D6A0',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }: any) {
+      addComponents({
+        '.container': {
+          margin: '0 auto',
+          maxWidth: 'calc(100% - 32px)',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1280px',
+          },
+          '@screen xl': {
+            maxWidth: '1400px',
+          },
+        },
+      });
+    },
+  ],
 };
 export default config;
